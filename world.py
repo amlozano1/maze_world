4ad76d90ae94968d2fb290c1a5e75447ff81d132
amlozano1 <lockes5hadow@gmail.com>
2014-02-14 17:26:06 -0700

    Playing with Pycharm, fixed some more pep8 things

--- world.py
+++ world.py
@@ -6,47 +6,48 @@ Created on Jan 18, 2014
 import random
 from collections import deque
 import math
-from serial import Serial
-from bitstring import BitArray
 from copy import copy
 
-blocks = {"empty" : 1,
-          "wall"  : 2,
+from bitstring import BitArray
+
+
+blocks = {"empty": 1,
+          "wall": 2,
           "hazard": 3,
           }
 headings = ["n", "s", "e", "w", "ne", "nw", "sw", "se"]
+
 
 class World(object):
     """
     classdocs
     """
 
-
-    def __init__(self, width= 10, length= 10, hieght=2, hear_port="COM5", see_port="COM5", feel_port="COM5"):
+    def __init__(self, width=10, length=10, hieght=2, hear_port="COM5", see_port="COM5", feel_port="COM5"):
         """
         Constructor
         """
         self.width = width
         self.length = length
         self.hieght = hieght
-        
+
         self.world_grid = [[[blocks['empty'] * hieght] * length] * width]
-        self.w, self.l, self.h = (0,0,0)
+        self.w, self.l, self.h = (0, 0, 0)
         self.goal = (width, length, hieght)
-        self.heading = deque( [
-                               (0, 1)  ,
-                               (-1, -1),
-                               (-1, 0) ,
-                               (-1, 1) ,
-                               (0, -1) ,
-                               (-1, -1),
-                               (1, 0)  ,
-                               (1, 1)  ,
-                               ]
-                        )
-        #self.hear_conn = Serial(hear_port, 115200)
-        #self.see_conn =  Serial(see_port, 115200)
-        #self.feel_conn = Serial(feel_port, 115200)
+        self.heading = deque([
+            (0, 1),
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (-1, -1),
+            (1, 0),
+            (1, 1),
+        ]
+        )
+        # self.hear_conn = Serial(hear_port, 115200)
+        # self.see_conn =  Serial(see_port, 115200)
+        # self.feel_conn = Serial(feel_port, 115200)
 
     @property
     def get_heading(self):
@@ -60,42 +61,44 @@ class World(object):
         Gets the current avatar position or the current position plus an offset
         :param off_w: w offset
         :param off_l: l offset
-        :param off_z: z offset
+        :param off_h: h offset
         :return: current pos val, plus offset
         """
         try:
             return self.world_grid[self.w + off_w][self.l + off_l][self.h + off_h]
         except IndexError:
             return blocks['wall']
-    
+
     def carve_path(self):
         """
         creates a random world of walls and cuts a path to the goal
         """
-        final = self.length # once we reach the last length, we set the goal and terminate
-        w, l, h = 0, 0, 0 # start at 0,0,0
-        last_move_name, last_move_tuple = "forward", (0,1,0) # we don't want to repeat the last movement
-        moves = {"back": (0,-1,0), "left": (-1, 0, 0), "right": (1, 0, 0), "up": (0,0,1), "down": (0,0,-1)} #possible moves
-        self.world_grid[w][l][h] = blocks["empty"] #set the current block empty
+        final = self.length  # once we reach the last length, we set the goal and terminate
+        w, l, h = 0, 0, 0  # start at 0,0,0
+        last_move_name, last_move_tuple = "forward", (0, 1, 0)  # we don't want to repeat the last movement
+        moves = {"back": (0, -1, 0), "left": (-1, 0, 0), "right": (1, 0, 0), "up": (0, 0, 1),
+                 "down": (0, 0, -1)}  # possible moves
+        self.world_grid[w][l][h] = blocks["empty"]  # set the current block empty
         while l != final:
-            move, (m_w, m_l, m_h) = random.choice(list(moves.iteritems())) #get a move
-            w += m_w  #apply move
+            move, (m_w, m_l, m_h) = random.choice(list(moves.iteritems()))  #  get a move
+            w += m_w  # apply move
             l += m_l
             h += m_h
-            self.world_grid[w][l][h] = blocks["empty"] #set that cell empty
-            moves[last_move_name] = last_move_tuple # add back in the last move to movelist
-            last_move_name, last_move_tuple = move, (m_w, m_l, m_h) #copy the current move to last move
-            moves.pop(last_move_name) #remove the current
-        self.goal = (w,l,h) #after terminating, set this as the goal
-            
+            self.world_grid[w][l][h] = blocks["empty"]  # set that cell empty
+            moves[last_move_name] = last_move_tuple  #  add back in the last move to movelist
+            last_move_name, last_move_tuple = move, (m_w, m_l, m_h)  # copy the current move to last move
+            moves.pop(last_move_name)  # remove the current
+        self.goal = (w, l, h)  # after terminating, set this as the goal
+
     def listen(self):
         """
-        searchs up and down for the distance to walls and returns the distance. Can also return block types in the future
+        searches up and down for the distance to walls and returns the distance. Can also return block types in the
+        future
         """
         up_distance, above = self.search(0, 0, 1)
         down_distance, below = self.search(0, 0, -1)
         return above, up_distance, below, down_distance
-    
+
     def search(self, w, l, h):
         """
         :param w: steps w per tick
@@ -104,13 +107,13 @@ class World(object):
         given a heading, continues in that direction until it finds a non-empty voxel
         @return distance, block_type
         """
-        c_w, c_l, c_h = w,l,h
+        c_w, c_l, c_h = w, l, h
         distance = 0
         while self.get_pos(c_w, c_l, c_h) == blocks['empty']:
             c_w += w
             c_l += l
             c_h += h
-        distance += int(math.floor(math.sqrt(c_w**2+c_l**2+c_h**2)))
+        distance += int(math.floor(math.sqrt(c_w ** 2 + c_l ** 2 + c_h ** 2)))
         return distance, self.get_pos(c_w, c_l, c_h)
 
     def look(self):
@@ -119,7 +122,7 @@ class World(object):
         """
         heading_queue = copy(self.heading)
         heading_queue.rotate(-2)
-        headings = list(heading_queue)[:5] #gives a list of 5 headings from leftmost to rightmost
+        headings = list(heading_queue)[:5]  # gives a list of 5 headings from leftmost to rightmost
         LED_list = []
         for w, l in headings:
             LED_list.append(self.search(w, l, 0))
@@ -130,14 +133,14 @@ class World(object):
         moves the avatar position in the direction of heading if is_forward is true, opposite direction otherwise
         :param is_forward: boolean flag for forward motion. Reverse motion if false
         """
-        wh, lh= self.get_heading
+        wh, lh = self.get_heading
         self.w += wh
         self.l += lh
         if self.get_pos() == blocks['wall']:
             self.w -= wh
             self.l -= lh
-            
-    
+
+
     def up_down(self, up):
         """
         move the avatar on the Z axis up or down
@@ -154,7 +157,7 @@ class World(object):
         self.h += up
         if self.get_pos() == blocks['wall']:
             self.h -= up
-    
+
     def turn(self, is_right):
         """
         turns the avatars heading by rotating the heading circular list
@@ -164,16 +167,16 @@ class World(object):
             self.heading.rotate(1)
         else:
             self.heading.rotate(-1)
-    
+
     def find_goal(self):
         """
         Get the heading for the goal
         
         """
         w, l, h = self.get_pos()
-        gw, gl, gh = self.goal 
+        gw, gl, gh = self.goal
         try:
-            angle_deg = angle((w,l),(gw,gl))
+            angle_deg = angle((w, l), (gw, gl))
         except ZeroDivisionError:
             if w > gw and l > gl:
                 return 2
@@ -191,32 +194,32 @@ class World(object):
             return 4
         else:
             return 5
-        
+
     def communicate(self):
-#        turn = self.hear_conn.read()
-        #self.up_down(self.hear_conn.read())
-        #above, up_distance, below, down_distance = self.listen()
+        # turn = self.hear_conn.read()
+        # self.up_down(self.hear_conn.read())
+        # above, up_distance, below, down_distance = self.listen()
         LEDs = self.look()
-        byte_seq = (BitArray("0b11") 
-                + BitArray(uint=LEDs[0][0], length=2)
-                + BitArray(uint=LEDs[0][1], length=4)
-                + BitArray(uint=LEDs[1][0], length=4)
-                + BitArray(uint=LEDs[1][1], length=4)
-                + BitArray(uint=LEDs[2][0], length=4)
-                + BitArray(uint=LEDs[2][1], length=4)
-                + BitArray(uint=LEDs[3][0], length=4)
-                + BitArray(uint=LEDs[3][1], length=4)
-                + BitArray(uint=LEDs[4][0], length=4)
-                + BitArray(uint=LEDs[4][1], length=4)
-                )
+        byte_seq = (BitArray("0b11")
+                    + BitArray(uint=LEDs[0][0], length=2)
+                    + BitArray(uint=LEDs[0][1], length=4)
+                    + BitArray(uint=LEDs[1][0], length=4)
+                    + BitArray(uint=LEDs[1][1], length=4)
+                    + BitArray(uint=LEDs[2][0], length=4)
+                    + BitArray(uint=LEDs[2][1], length=4)
+                    + BitArray(uint=LEDs[3][0], length=4)
+                    + BitArray(uint=LEDs[3][1], length=4)
+                    + BitArray(uint=LEDs[4][0], length=4)
+                    + BitArray(uint=LEDs[4][1], length=4)
+        )
         self.hear_conn.write(byte_seq.bytes)
         print self.see_conn.read()
+
 
 def angle(pt1, pt2):
     x1, y1 = pt1
     x2, y2 = pt2
-    inner_product = x1*x2 + y1*y2
+    inner_product = x1 * x2 + y1 * y2
     len1 = math.hypot(x1, y1)
     len2 = math.hypot(x2, y2)
-    return math.acos(inner_product/(len1*len2))
-   
+    return math.acos(inner_product / (len1 * len2))
